818aa36287d4dba1a7ae8497d0374673aa71c670
hmburd <h.m.burd@gmail.com>
2019-07-31 20:13:00 -0500

    reversed the comments and addes says to guestbook

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react'
 class CommentList extends Component {
   getComments = () => {
     let comments_display = [] 
-    for (let i=0; i<this.props.comments.length; i++){
-      comments_display.push(<p>{this.props.comments[i].commenter_name + ': ' + this.props.comments[i].comment_text}</p>)
+    for (let i = 0; i<this.props.comments.length; i++){
+      comments_display.push(<p>{this.props.comments[i].commenter_name + ' says:  ' + this.props.comments[i].comment_text}</p>)
     }
-    return comments_display
+    return comments_display.reverse()
   }
   render() {
 
